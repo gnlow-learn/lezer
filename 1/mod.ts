988c7164a8778e053ac1fcd3f83f4838c120778b
Gnlow <gnlow@pm.me>
2025-04-13 02:10:56 +0900

    string

--- 1/mod.ts
+++ 1/mod.ts
@@ -1,21 +1,7 @@
 import { parser } from "./parser.js"
 
 const tree = parser.parse(`
-    fun cloop() {
-        if (rdrec() == 0) {
-            endfil()
-        }
-        wrrec()
-        cloop()
-    }
-
-    buffer = resb(4096)
-
-    fun endfil(a, b) {
-        if (buffer == eof) {
-
-        }
-    }
+print("Hello, World!")
 `)
 
 let depth = 0
